49c35d14ea85b9029374199548c12b6cc51deaad
Kirk Abbott <kirkabbott1@gmail.com>
2017-01-09 16:57:00 -0500

    adds python mergedMeeting solution

---
+++ mergedMeeting.py
@@ -0,0 +1,21 @@
+  def merge_ranges(meetings):
+
+    # sort by start times
+    sorted_meetings = sorted(meetings)
+
+    # initialize merged_meetings with the earliest meeting
+    merged_meetings = [sorted_meetings[0]]
+
+    for current_meeting_start, current_meeting_end in sorted_meetings[1:]:
+
+        last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+        # if the current and last meetings overlap, use the latest end time
+        if (current_meeting_start <= last_merged_meeting_end):
+            merged_meetings[-1] = (last_merged_meeting_start, max(last_merged_meeting_end, current_meeting_end))
+
+        # add the current meeting since it doesn't overlap
+        else:
+            merged_meetings.append((current_meeting_start, current_meeting_end))
+
+    return merged_meetings
