c4cfaafdc9ce172a3f3e41f94da792c3be53aa0a
Kirk Abbott <kirkabbott1@gmail.com>
2017-01-09 16:53:59 -0500

    adds mergedMeeting solution

--- mergedMeeting.js
+++ mergedMeeting.js
@@ -67,3 +67,47 @@ solution
 //
 // We can merge the current meeting with the previous one, so we do.
 // We can't merge the current meeting with the previous one, so we know the previous meeting can't be merged with any future meetings and we throw the current meeting into mergedMeetings.
+
+function mergeRanges(meetings) {
+
+  // sort by start times
+  var sortedMeetings = meetings.slice().sort(function(a, b) {
+      return a.startTime > b.startTime ? 1 : -1;
+  });
+
+  // initialize mergedMeetings with the earliest meeting
+  var mergedMeetings = [sortedMeetings[0]];
+
+  for (var i = 1; i < sortedMeetings.length; i++) {
+
+      var currentMeeting    = sortedMeetings[i];
+      var lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+      // if the current and last meetings overlap, use the latest end time
+      if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+          lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime);
+
+      // add the current meeting since it doesn't overlap
+      } else {
+          mergedMeetings.push(currentMeeting);
+      }
+  }
+
+  return mergedMeetings;
+}
+Complexity
+// O(n\lg{n})O(nlgn) time and O(n)O(n) space.
+//
+// Even though we only walk through our array of meetings once to merge them, we sort all the meetings first, giving us a runtime of O(n\lg{n})O(nlgn). It's worth noting that if our input were sorted, we could skip the sort and do this in O(n)O(n) time!
+//
+// We create a new array of merged meeting times. In the worst case, none of the meetings overlap, giving us an array identical to the input array . Thus we have a worst-case space cost of O(n)O(n).
+
+// This one arguably uses a greedy ↴ approach as well, except this time we had to sort the array first.
+//
+// How did we figure that out?
+//
+// We started off trying to solve the problem in one pass, and we noticed that it wouldn't work. We then noticed the reason it wouldn't work: to see if a given meeting can be merged, we have to look at all the other meetings! That's because the order of the meetings is random.
+//
+// That's what got us thinking: what if the array were sorted? We saw that then a greedy approach would work. We had to spend O(n\lg{n})O(nlgn) time on sorting the array , but it was better than our initial brute force approach, which cost us O(n^2)O(n
+// ​2
+// ​​ ) time!
